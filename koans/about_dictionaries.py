7970e593a0eeac470f9dd5351b749b7b2eff111a
Anurag Vaishwade <anuragvaishwade@gmail.com>
2014-01-16 18:27:56 +0530

    Fixed koan 29 outputs Although never is often better than right now.

--- koans/about_dictionaries.py
+++ koans/about_dictionaries.py
@@ -12,29 +12,29 @@ class AboutDictionaries(Koan):
         empty_dict = dict()
         self.assertEqual(dict, type(empty_dict))
         self.assertEqual(dict(), empty_dict)
-        self.assertEqual(__, len(empty_dict))
+        self.assertEqual(0, len(empty_dict))
         
     def test_dictionary_literals(self):
         babel_fish = { 'one': "uno", 'two': "dos" }
-        self.assertEqual(__, len(babel_fish))
+        self.assertEqual(2, len(babel_fish))
         
     def test_accessing_dictionaries(self):
         babel_fish = { 'one': "uno", 'two': "dos" }
-        self.assertEqual(__, babel_fish['one'])
-        self.assertEqual(__, babel_fish['two'])
+        self.assertEqual("uno", babel_fish['one'])
+        self.assertEqual("dos", babel_fish['two'])
         
     def test_changing_dictionaries(self):
         babel_fish = { 'one': "uno", 'two': "dos" }
         babel_fish['one'] = "eins"
         
-        expected = { 'two': "dos", 'one': __ }
+        expected = { 'two': "dos", 'one': "eins" }
         self.assertEqual(expected, babel_fish)
         
     def test_dictionary_is_unordered(self):
         dict1 = { 'one': "uno", 'two': "dos" }
         dict2 = { 'two': "dos", 'one': "uno" }
         
-        self.assertEqual(____, dict1 == dict2)
+        self.assertEqual(True, dict1 == dict2)
         
     def test_dictionary_keys(self):
         babel_fish = { 'one': "uno", 'two': "dos" }
