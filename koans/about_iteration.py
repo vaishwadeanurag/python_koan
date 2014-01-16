31638b0168732ddb1487f49bef3d4654686637e9
Anurag Vaishwade <anuragvaishwade@gmail.com>
2014-01-16 22:55:44 +0530

    Fixed Koan 120

--- koans/about_iteration.py
+++ koans/about_iteration.py
@@ -87,8 +87,8 @@ class AboutIteration(Koan):
         
         comprehension = [delicacy.capitalize() for delicacy in feast]
         
-        self.assertEqual(__, comprehension[0])
-        self.assertEqual(__, comprehension[2])
+        self.assertEqual('Lambs', comprehension[0])
+        self.assertEqual('Orangutans', comprehension[2])
         
     def test_use_pass_for_iterations_with_no_body(self):
         for num in range(1,5):
