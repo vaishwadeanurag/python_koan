6aedc49a4691ab0e945106748bcb3001c6f0683e
Anurag Vaishwade <anuragvaishwade@gmail.com>
2014-01-16 22:16:35 +0530

    Fixed koan 80

--- koans/about_methods.py
+++ koans/about_methods.py
@@ -108,7 +108,7 @@ class AboutMethods(Koan):
         "shall"
         "not"
         pass
-        self.assertEqual(____, "Still got to this line" != None)
+        self.assertEqual(True, "Still got to this line" != None)
 
     # ------------------------------------------------------------------
 
