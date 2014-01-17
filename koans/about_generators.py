7f80db976990d587aeaa95cdda15dc9c5b2a6e0f
Anurag Vaishwade <anuragvaishwade@gmail.com>
2014-01-17 23:57:59 +0530

    Fixed Koan 132-133

--- koans/about_generators.py
+++ koans/about_generators.py
@@ -132,13 +132,13 @@ class AboutGenerators(Koan):
                          
         generator2 = self.yield_tester()
         next(generator2)
-        self.assertEqual(__, next(generator2))
+        self.assertEqual('no value', next(generator2))
 
     def test_send_none_is_equivelant_to_next(self):
         generator = self.yield_tester()
         
         next(generator)
         # 'next(generator)' is exactly equivelant to 'generator.send(None)'
-        self.assertEqual(__, generator.send(None))
+        self.assertEqual('no value', generator.send(None)) #Type none will act as it is called without a value
 
  
