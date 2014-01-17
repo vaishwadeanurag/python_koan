abc9b403a7fb37f4bbae17e93dbd24c121aa7999
Anurag Vaishwade <anuragvaishwade@gmail.com>
2014-01-17 23:33:30 +0530

    Fixed Koan 128

--- koans/about_generators.py
+++ koans/about_generators.py
@@ -72,7 +72,7 @@ class AboutGenerators(Koan):
 
     def test_generator_method_with_parameter(self):
         result = self.square_me(range(2,5))
-        self.assertEqual(__, list(result))
+        self.assertEqual([4,9,16], list(result))
 
     # ------------------------------------------------------------------
 
